ffaf2389422227d18b69611063df8a46c96546f8
herschdorfer <herschdorfer@gmail.com>
2020-04-16 22:57:24 +0200

    added Arduino MKR WiFi 1010

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 
+#define DEBUG
+#define DEBUGDELAY 10000
 
 #include "config.h"
 #include <Arduino.h>
@@ -6,38 +8,43 @@
 #include <Adafruit_BME280.h>
 #include <WiFi101.h>
 #include <WiFiClient.h>
-#include <RTCZero.h>
 #include <PubSubClient.h>
 #include <Adafruit_Sensor.h>
+
+#ifndef DEBUG
+#include <RTCZero.h>
 #include <Adafruit_SleepyDog.h>
+#endif
 
 #define CMD_INTERVAL 500
 
-#define TEMPERATURE_TOPIC 	"kitchen/sensors/temperatureout"
-#define HUMIDITY_TOPIC 		"kitchen/sensors/humidityout"
-#define VOLTAGE_TOPIC 		"kitchen/sensors/voltageout"
-#define PRESSURE_TOPIC 		"kitchen/sensors/pressureout"
+#define DEVICE_NAME "OutsideThermometer"
+
+//#define VOLTAGE
+
+#define TEMPERATURE_TOPIC   "living_room/sensors/temperatureout"
+#define HUMIDITY_TOPIC      "living_room/sensors/humidityout"
+#define VOLTAGE_TOPIC       "living_room/sensors/voltageout"
 
 Adafruit_BME280 bme; // I2C
+
+#ifndef DEBUG
 RTCZero rtc;
+#endif
 
 // Set some dummy data, since we just want intervals.
 const uint8 seconds = 0;
 const uint8 minutes = 0;
-const uint8 hours = 0;
-const uint8 day = 1;
-const uint8 month = 1;
-const uint8 year = 17;
+const uint8 hours   = 0;
+const uint8 day     = 1;
+const uint8 month   = 1;
+const uint8 year    = 17;
 
 #define ANALOG_BITS 12
-#define BITS 4096
-#define VREF 3.3
-#define R1 456
-#define R2 978
-#define SEALEVELPRESSURE_HPA (1013.25)
-
-#define DEBUG
-#define DEBUGDELAY 60000
+#define BITS        4096
+#define VREF        3.3
+#define R1          456
+#define R2          978
 
 WiFiClient wifiClient;
 PubSubClient mqtt(wifiClient);
@@ -45,161 +52,167 @@ PubSubClient mqtt(wifiClient);
 unsigned long delayTime;
 
 void getNextSample(float *Temperature,
-				   float *Humidity,
-				   float *Pressure)
+                   float *Humidity)
 {
-	bme.takeForcedMeasurement();
-	
-	*Humidity    = bme.readHumidity();
-	*Temperature = bme.readTemperature();
-	//*Pressure    = bme.readPressure() / 100.0F;
+  bme.takeForcedMeasurement();
+
+  *Humidity    = bme.readHumidity();
+  *Temperature = bme.readTemperature();
 }
 
 void connectWifi()
 {
-	Serial.print("WiFi connecting ");
-	WiFi.begin(WIFI_SSID, WIFI_PASS);
-	while (WiFi.status() != WL_CONNECTED)
-	{
-		Serial.print(".");
-		delay(CMD_INTERVAL);
-	}
+  Serial.print("WiFi connecting ");
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(CMD_INTERVAL);
+  }
 
-	Serial.print(" connected\r\n");
+  Serial.print(" connected\r\n");
 }
 
 void disconnectWifi()
 {
-	Serial.print("WiFi disconnecting ");
-	
-	WiFi.disconnect();
-	while (WiFi.status() == WL_CONNECTED)
-	{
-		delay(CMD_INTERVAL);
-		Serial.print(".");
-	}
-	Serial.print(" disconnected\r\n");
+  Serial.print("WiFi disconnecting ");
+
+  WiFi.disconnect();
+  while (WiFi.status() == WL_CONNECTED)
+  {
+    delay(CMD_INTERVAL);
+    Serial.print(".");
+  }
+  Serial.print(" disconnected\r\n");
 }
 
 void connectMQTT()
 {
-	Serial.print("MQTT connecting ");
-	while (!mqtt.connected())
-	{
-		if (mqtt.connect("OutsideThermometer"))
-		{
-			Serial.print(" connected");
-		}
-		else
-		{
-			delay(CMD_INTERVAL);
-			Serial.print(".");
-		}
-	}
-	Serial.println("");
+  Serial.print("MQTT connecting ");
+  while (!mqtt.connected())
+  {
+    if (mqtt.connect(DEVICE_NAME))
+    {
+      Serial.print(" connected");
+    }
+    else
+    {
+      delay(CMD_INTERVAL);
+      Serial.print(".");
+    }
+  }
+  Serial.println("");
 }
 
 void disconnectMQTT()
 {
-	Serial.print("MQTT disconnecting ");
-	
-	mqtt.disconnect();
-	while(mqtt.connected())
-	{
-		delay(CMD_INTERVAL);
-		Serial.print(".");
-	}
-	Serial.print(" disconnected\r\n");
+  Serial.print("MQTT disconnecting ");
+
+  mqtt.disconnect();
+  while(mqtt.connected())
+  {
+    delay(CMD_INTERVAL);
+    Serial.print(".");
+  }
+  Serial.print(" disconnected\r\n");
 }
 
 void setup()
 {
 
-    Serial.begin(9600);
-    
-	analogReadResolution(ANALOG_BITS);
+  Serial.begin(9600);
 
-	mqtt.setServer(MQTT_SERVER, MQTT_SERVERPORT);
+  analogReadResolution(ANALOG_BITS);
 
-	pinMode(6UL, OUTPUT);
-	WiFi.hostname("OutsideThermometer");
-	bme.begin(0x76, &Wire);
+  mqtt.setServer(MQTT_SERVER, MQTT_SERVERPORT);
 
-    bme.setSampling(Adafruit_BME280::MODE_FORCED,
-                    Adafruit_BME280::SAMPLING_X1, // temperature
-                    Adafruit_BME280::SAMPLING_NONE, // pressure
-                    Adafruit_BME280::SAMPLING_X1, // humidity
-                    Adafruit_BME280::FILTER_OFF   );
+  pinMode(6UL, OUTPUT);
+  WiFi.hostname("OutsideThermometer");
+  bme.begin(0x76, &Wire);
+
+  bme.setSampling(Adafruit_BME280::MODE_FORCED,
+                  Adafruit_BME280::SAMPLING_X1, // temperature
+                  Adafruit_BME280::SAMPLING_NONE, // pressure
+                  Adafruit_BME280::SAMPLING_X1, // humidity
+                  Adafruit_BME280::FILTER_OFF   );
 
 #ifndef DEBUG
-	// Set the RTC
-	rtc.begin();
-	rtc.setTime(hours, minutes, seconds);
-	rtc.setDate(day, month, year);
-	rtc.enableAlarm(rtc.MATCH_MMSS);
+  // Set the RTC
+  rtc.begin();
+  rtc.setTime(hours, minutes, seconds);
+  rtc.setDate(day, month, year);
+  rtc.enableAlarm(rtc.MATCH_MMSS);
 #endif
 }
 
 void work()
 {
-	float temperature, humidity, pressure;
+  float temperature, humidity;
 
-	digitalWrite(6UL, HIGH);
-	getNextSample(&temperature, &humidity, &pressure);
+  digitalWrite(6UL, HIGH);
+  getNextSample(&temperature, &humidity);
 
-	Serial.print("Humidity: 	");
-	Serial.print(humidity);
-	Serial.print(" %\r\n");
+  Serial.print("Humidity:      ");
+  Serial.print(humidity);
+  Serial.print(" %\r\n");
 
-	Serial.print("Temperature: 	");
-	Serial.print(temperature);
-	Serial.print(" °C\r\n");
+  Serial.print("Temperature:   ");
+  Serial.print(temperature);
+  Serial.print(" °C\r\n");
 
-	//Serial.print("Pressure: 	");
-	//Serial.print(pressure);
-	//Serial.print(" mBar\r\n");
-	Watchdog.reset();
+#ifdef VOLTAGE
+  uint16 Vin = analogRead(A1);
+  float Vout = ((double)Vin * VREF / BITS) * (R1 + R2) / R2;
+  Serial.print("VBAT:     ");
+  Serial.print(Vout);
+  Serial.print(" V\r\n");
+#endif
 
-	uint16 Vin = analogRead(A1);
-	float Vout = ((double)Vin * VREF / BITS) * (R1 + R2) / R2;
-	Serial.print("VBAT: 		");
-	Serial.print(Vout);
-	Serial.print(" V\r\n");
+#ifndef DEBUG
+  Watchdog.reset();
+#endif
 
-	connectWifi();
-	connectMQTT();
+  connectWifi();
+  connectMQTT();
 
-	Watchdog.reset();
+#ifndef DEBUG
+  Watchdog.reset();
+#endif
 
-	Serial.print("Sending ... ");
-	mqtt.publish(TEMPERATURE_TOPIC, String(temperature).c_str(), 	true);
-	mqtt.publish(HUMIDITY_TOPIC, 	String(humidity).c_str(), 		true);
-	mqtt.publish(VOLTAGE_TOPIC, 	String(Vout).c_str(), 			true);
-	mqtt.publish(PRESSURE_TOPIC, 	String(pressure).c_str(), 		true);
-	Serial.print("done\r\n");
+  Serial.print("Sending ... ");
+  mqtt.publish(TEMPERATURE_TOPIC, String(temperature).c_str(),  true);
+  mqtt.publish(HUMIDITY_TOPIC,    String(humidity).c_str(),     true);
+#ifdef VOLTAGE
+  mqtt.publish(VOLTAGE_TOPIC,     String(Vout).c_str(),         true);
+#endif
+  Serial.print("done\r\n");
 
-	Watchdog.reset();
+#ifndef DEBUG
+  Watchdog.reset();
+#endif
 
-	disconnectMQTT();
-	disconnectWifi();
-	digitalWrite(6, LOW);
+  disconnectMQTT();
+  disconnectWifi();
+  digitalWrite(6, LOW);
 }
 
 void loop() {
 
-	Serial.println("-------------------------");
+  Serial.begin(9600);
+
+  Serial.println("-------------------------");
 
 #ifndef DEBUG
-	Watchdog.enable(8000);
+  Watchdog.enable(8000);
 #endif
 
-	work(); // do the work
-	
+  work(); // do the work
+
 #ifdef DEBUG
-	delay(DEBUGDELAY);
-#else	
-	rtc.setAlarmMinutes((rtc.getAlarmMinutes() + SENDING_INTERVAL) % 60);
-	Watchdog.disable();
-	rtc.standbyMode();
+  delay(DEBUGDELAY);
+#else
+  rtc.setAlarmMinutes((rtc.getAlarmMinutes() + SENDING_INTERVAL) % 60);
+  Watchdog.disable();
+  rtc.standbyMode();
 #endif
 }
